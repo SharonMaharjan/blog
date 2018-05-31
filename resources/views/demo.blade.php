dcc797b26390dda1fa3d44e538bb0aecf3426c29
SharonMaharjan <sharonmaharjan18@gmail.com>
2018-05-31 11:57:18 +0545

    hey

---
+++ resources/views/demo.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Demo</title>
+</head>
+<body>
+	<h1>Smile</h1>
+	
+
+</body>
+</html>
---
+++ resources/views/demo2.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>happy you</title>
+</head>
+<body>
+<h2>peaceeeee</h2>
+</body>
+</html>>
